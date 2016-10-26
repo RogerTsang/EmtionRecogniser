9d2894a7b0d3baa139e5d987eda72b1ab80bc8f5
defuyun <carlliu2012@gmail.com>
2016-10-26 17:25:05 +1100

    added testing class

--- Recogniser.py
+++ Recogniser.py
@@ -78,22 +78,12 @@ class Recognizer(object):
                     else:
                         disp_minor_emotion[i] = ''
 
-                    # major_max_count = 0
-                    # for _emotion, _count in cache[i].items():
-                    #     if _count > major_max_count:
-                    #         disp_major_emotion[i] = _emotion
-                    #         major_max_count = _count
-                    # minor_max_count = 0
-                    #
                     major_text_color = (0, 0, 255)
                     minor_text_color = (50, 50, 150)
                     text_font = cv2.FONT_HERSHEY_SIMPLEX
                     cv2.putText(color_frame, str(disp_major_emotion[i]), (x, y), text_font, 1, major_text_color, 3)
                     cv2.putText(color_frame, str(disp_minor_emotion[i]), (x, y - 40), text_font, 0.8, minor_text_color, 2)
 
-                    # cache_emop.append((x, y))
-                    # print "pic %s analysis Emotion: %10s | Confidence: %10f" % (i, disp_emotion[i], confidence)
-                    
                     if i == len(results) - 1:
                         cache = list(dict())
 
@@ -112,51 +102,12 @@ class Recognizer(object):
                 cv2.putText(color_frame, str(disp_major_emotion[i]), (x, y), text_font, 1, major_text_color, 3)
                 cv2.putText(color_frame, str(disp_minor_emotion[i]), (x, y - 40), text_font, 0.8, minor_text_color, 2)
 
-            # if len(results) == 0:
-            #     # logging.debug('emopo: %s', cache_emop)
-            #     for i, emp in enumerate(cache_emop):
-            #         if i >= len(disp_major_emotion):
-            #             break
-            #         cv2.putText(color_frame, str(disp_major_emotion[i]), (emp[0], emp[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
             cv2.imshow('face', color_frame)
             k = cv2.waitKey(10) & 0xff
             if k == 27:
                 capture.release()
                 cv2.destroyAllWindows()
                 return 0
-                
-    def eval(self, image, correct_emotion):
-        results = self._sampler.extract(color_frame)
-        emotion = None
-        for i, (gray_resize, x, y) in enumerate(results):
-            prediction, confidence = recogniser.predict(gray_resize)
-            emotion = self._emotion_dictionary[prediction]
-        if emotion is None:
-            return 2
-
-        return 1 if emotion == correct_emotion else 0
-
-    def benchmark(self, filepath=os.path.join(CKPath_Root, CKPath_Benchmark)):
-        benchmark_flies = glob.glob(os.path.join(filepath, '*'))
-        predict_true = 0
-        predict_false = 0
-        not_reco = 0
-
-        for file in benchmark_flies:
-            emotion = os.path.basename(file)
-            for images in glob.glob(os.path.join(file, '*')):
-                image = cv2.imread(images)
-                res = self.eval(image, emotion)
-                if res == 1:
-                    predict_true += 1
-                elif res == 0:
-                    predict_false += 1
-                else:
-                    not_reco += 1
-
-        print("Rate of success: ", predict_true/(predict_true + predict_false))
-        print("%s miss" % not_reco)
 
 if __name__ == '__main__':
     # rt_flag = False
---
+++ Tester.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+
+import cv2
+import os
+import numpy as np
+
+from config import *
+from Sampler import Sampler
+from Trainer import Trainer
+
+class Tester(object):
+    def __init__(self, s, t):
+        self._sampler = s
+        self._emotion_dictionary = emotions
+        self._recogniser = t.get_recognizer(False)
+
+    def eval(self, image, correct_emotion):
+        results = self._sampler.extract(color_frame)
+        emotion = None
+        correct = None
+        for i, (gray_resize, x, y) in enumerate(results):
+            prediction, confidence = self.recogniser.predict(gray_resize)
+            emotion = self._emotion_dictionary[prediction]
+            if(emotion == correct_emotion):
+                correct = True
+            else:
+                corrent = False
+
+        return (emotion, correct)
+
+    def static_test(self, filepath=os.path.join(CKPath_Root, CKPath_Benchmark)):
+        benchmark_flies = glob.glob(os.path.join(filepath, '*'))
+        predict_true = 0
+        predict_false = 0
+        not_reco = 0
+
+        confusion_matrix = list(list())
+        for i in range(5):
+            confusion_matrix.push_back([0] * 5)
+
+        for file in benchmark_flies:
+            emotion = os.path.basename(file)
+            for images in glob.glob(os.path.join(file, '*')):
+                image = cv2.imread(images)
+                pred_emotion, res = self.eval(image, emotion)
+
+                if res == True:
+                    predict_true += 1
+                elif res == False:
+                    predict_false += 1
+                else:
+                    not_reco += 1
+
+                confusion_matrix[emotion][pred_emotion] += 1
+
+        print("Rate of success: ", predict_true/(predict_true + predict_false))
+        print("%s miss" % not_reco)
+
+    def live_test(self):
+        capture = cv2.VideoCapture(0)
+        confusion_matrix = list(list())
+        recogniser = self._trainer.get_recognizer(False)
+        cache = dict({'neutral':0})
+
+        for i in range(5):
+            confusion_matrix.push_back([0] * 5)
+
+        start = time.time()
+        time.clock()
+
+        while True:
+            ret, color_frame = capture.read()
+            results = self._sampler.extract(color_frame)
+
+            for i, (gray_resize, x, y) in enumerate(results):
+                prediction, confidence = recogniser.predict(gray_resize)
+                emotion = self._emotion_dictionary[prediction]
+
+                if time.time() - start >= 1:
+                    cache = dict({'neutral':0})
+                else:
+                    if cache.get(emotion, None) is None:
+                        cache[emotion] = 1
+                    else:
+                        cache[emotion] += 1
+
+            cv2.imshow('face', color_frame)
+            k = cv2.waitKey(30) & 0xff
+            if 56 >= k >= 48:
+                correct_emotion = k - 48
+                pred_emotion = 'neutral'
+                count = 0
+
+                for key, item in cache.items():
+                    if item > count:
+                        maxim = key
+                        count = item
+
+                if count == 0:
+                    continue
+
+                if correct_emotion == pred_emotion:
+                    predict_true += 1
+                else:
+                    predict_false += 1
+
+                confusion_matrix[correct_emotion][pred_emotion] += 1
+
+            elif k == 27:
+                capture.release()
+                cv2.destroyAllWindows()
+                print("Rate of success: ", predict_true/(predict_true + predict_false))
+                return confusion_matrix
+
+if __name__ == '__main__':
+    s = Sampler()
+    t = Trainer('Eigen')
+    tes = Tester(s,t)
+
+    tes.static_test()
